03420ae2384410eea84518ba8718d12bf4309396
SamNgigi <sam.t.ngigi@gmail.com>
2019-01-15 04:18:04 +0300

    MoodFragment getting the appropriate view

--- app/src/main/java/com/hai/jedi/gridviewpractice/MoodDialogFragment.java
+++ app/src/main/java/com/hai/jedi/gridviewpractice/MoodDialogFragment.java
@@ -9,9 +9,16 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+
+import android.util.Log;
 
 
 public class MoodDialogFragment extends DialogFragment{
+
+    public static final String TAG = MoodDialogFragment.class.getSimpleName();
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
     {
@@ -34,6 +41,30 @@ public class MoodDialogFragment extends DialogFragment{
         });
 
 
+        /**
+         * To pull information from our fragment, we first have to
+         * 1. Pull Radio Buttons from our Radio group.
+         * 2. Get Radio Button id
+         * 3. Return value from Radio Button
+         * 4. Close DialogFragment*/
+
+
+        submitButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                /**
+                 * Adding this code here to the onClick makes sure that we get the appropriate radio
+                 * button that was clicked*/
+                // Pulling Radio Button Group
+                RadioGroup moodSurvey = rootView.findViewById(R.id.moodRadioGroup);
+                // Get Radio Button Id that user has selected.
+                int selectedId = moodSurvey.getCheckedRadioButtonId();
+                // Return value of above RadioButton
+                final RadioButton selectedButtonValue = rootView.findViewById(selectedId);
+                Log.d(TAG, "TESTING: "+ selectedButtonValue.getText().toString());
+                dismiss();
+            }
+        });
 
 
         return rootView;
