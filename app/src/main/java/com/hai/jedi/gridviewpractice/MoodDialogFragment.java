27967b3ca5cb434b143f90f2e461c5332796c071
SamNgigi <sam.t.ngigi@gmail.com>
2019-01-15 04:53:53 +0300

    Dialog Builder pattern working

--- app/src/main/java/com/hai/jedi/gridviewpractice/MoodDialogFragment.java
+++ app/src/main/java/com/hai/jedi/gridviewpractice/MoodDialogFragment.java
@@ -2,6 +2,11 @@ package com.hai.jedi.gridviewpractice;
 
 import androidx.fragment.app.DialogFragment;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+
+import android.content.DialogInterface;
+
 import android.os.Bundle;
 
 import android.view.LayoutInflater;
@@ -15,60 +20,35 @@ import android.widget.RadioGroup;
 import android.util.Log;
 
 
-public class MoodDialogFragment extends DialogFragment{
-
-    public static final String TAG = MoodDialogFragment.class.getSimpleName();
-
+public class MoodDialogFragment extends DialogFragment {
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
-    {
-        View rootView = inflater.inflate(R.layout.fragment_mood_dialog, container, false);
-        getDialog().setTitle("Simple Dialog");
+    public Dialog onCreateDialog(Bundle savedInstanceSate){
 
-        // On a Fragment we cannot use butter-knife because we are not interacting with the activity
-        // We do it old school
+        // Creating the dialog builder object
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        // Adding a title to the builder
+        builder.setTitle("Dialog view a Dialog Builder");
+        // Adding a message to the builder
+        builder.setMessage("Would you like to take a survey");
 
-        Button cancelButton = rootView.findViewById(R.id.cancelButton);
-        Button submitButton = rootView.findViewById(R.id.submitButton);
+        // Creating the Yes button and adding a click event handler.
+        builder.setPositiveButton("Yes", new DialogInterface.OnClickListener(){
+           @Override
+           public void onClick(DialogInterface dialog, int which){
+               dismiss();
+           }
+        });
 
-        // We cannot use the View.onClickListener either because we cannot depend on an activity
-        // only a fragment view. We do clicks old school.
-        cancelButton.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View view){
-                dismiss();
-            }
+        //Creating the No button and adding a click event handler
+        builder.setNegativeButton("Nope", new DialogInterface.OnClickListener (){
+           @Override
+           public void onClick(DialogInterface dialog, int which){
+               dismiss();
+           }
         });
 
 
-        /**
-         * To pull information from our fragment, we first have to
-         * 1. Pull Radio Buttons from our Radio group.
-         * 2. Get Radio Button id
-         * 3. Return value from Radio Button
-         * 4. Close DialogFragment*/
 
-
-        submitButton.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v){
-                /**
-                 * Adding this code here to the onClick makes sure that we get the appropriate radio
-                 * button that was clicked*/
-                // Pulling Radio Button Group
-                RadioGroup moodSurvey = rootView.findViewById(R.id.moodRadioGroup);
-                // Get Radio Button Id that user has selected.
-                int selectedId = moodSurvey.getCheckedRadioButtonId();
-                // Return value of above RadioButton
-                final RadioButton selectedButtonValue = rootView.findViewById(selectedId);
-                Log.d(TAG, "TESTING: "+ selectedButtonValue.getText().toString());
-                dismiss();
-            }
-        });
-
-
-        return rootView;
+        return builder.create();
     }
-
-
 }
